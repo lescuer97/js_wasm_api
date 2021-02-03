b12dde47955cb66ad7ead95624e0d2ef205e9e6f
lescuer97 <leoescuer@protonmail.com>
2021-02-03 10:50:28 +0100

    Errores en el codigo
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
-console.log("hello world");
+
+const wasm = await Deno.readFile("./wasm_test/pkg/wasm_test_bg.wasm");
+
+
+
+
+const wasmModule = new WebAssembly.Module(wasm);
+
+const wasmInstance = new WebAssembly.Instance(wasmModule);
+
+const wasm_test = wasmInstance.exports;
+// TODO POR ALGUNA RAZON GREET NO ES EJECTUTABLE
+console.log( wasm_test.greet(1, 3));
 
--- wasm_test/src/lib.rs
+++ wasm_test/src/lib.rs
@@ -1,11 +1,8 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn greet(a: i8, b:i8) -> i8  {
+   let result = a + b;
+   return result;
 }
